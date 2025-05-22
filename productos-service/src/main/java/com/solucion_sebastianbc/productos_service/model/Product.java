6b53149296eb6195efcb208b0673eb578872d9a7
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-22 16:35:13 -0500

    feat(productos): Define Producto entity and configure persistence

---
+++ productos-service/src/main/java/com/solucion_sebastianbc/productos_service/model/Product.java
@@ -0,0 +1,32 @@
+package com.solucion_sebastianbc.productos_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table( name = "productos")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @NotNull
+    @Column(name = "precio", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+}
